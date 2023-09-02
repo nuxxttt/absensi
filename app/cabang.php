5c3334cb7bb1aa784a99bf0cd68335f04a7a71cc
manggala56 <manggalaputra740@gmail.com>
2023-09-02 14:27:33 +0700

    frist commit

---
+++ app/cabang.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class cabang extends Model
+{
+    use HasFactory;
+}
---
+++ app/lembur.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class lembur extends Model
+{
+    use HasFactory;
+}
