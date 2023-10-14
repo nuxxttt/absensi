c68cdc56eedf109e6a7d37644e2b2993e8841a32
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2023-10-15 02:30:51 +0700

    new fitur

--- app/Imports/SolutionsImport.php
+++ app/Imports/SolutionsImport.php
@@ -6,17 +6,11 @@ use App\AbsenModel;
 use App\ShiftModel;
 use App\KaryawanModel;
 use Carbon\Carbon;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
 class SolutionsImport implements ToCollection
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function  collection(Collection $collection)
+    public function collection(Collection $collection)
     {
         foreach($collection as $item){
             // data id 0
@@ -26,41 +20,41 @@ class SolutionsImport implements ToCollection
                 $data = $item[1];
                 $data = strtotime($data);
                 $carbonDate = Carbon::createFromTimestamp(($data - 25569) * 86400);
-                if(!empty($data)){
-                    $absen_time =$carbonDate->format('H:m');
-                    $absen_tanggal = $carbonDate->format('Y:m:d');
-                    $check_id = AbsenModel::where('tanggal',$absen_tanggal)->where('id_pegawai',$item[0])->value('id');
+                if($data !== false){
+                    $absen_time = $carbonDate->format('H:i'); // Perbaikan: Menggunakan "i" untuk menampilkan menit
+                    $absen_tanggal = $carbonDate->format('Y-m-d');
+                    $check_id = AbsenModel::where('tanggal', $absen_tanggal)->where('id_pegawai', $item[0])->value('id');
                     if(!empty($check_id)){
-                        $id_shift = KaryawanModel::where('id_absen',$item[0])->value('id_shift');
-                        $shift_pulang = ShiftModel::where('id',$id_shift)->value('jam_pulang');
-                            if($absen_time > $shift_pulang){
-                                $status = AbsenModel::where('tanggal',$absen_tanggal)->where('id_pegawai',$item[0])->value('status');
-                                if($status == "tepat_waktu"){
-                                    AbsenModel::where('id',$check_id)->update([
-                                        'absen_pulang'=>$absen_time,
-                                        'status'=>'tidak_tepat_waktu'
-                                    ]);
-                                }
+                        $id_shift = KaryawanModel::where('id_absen', $item[0])->value('id_shift');
+                        $shift_pulang = ShiftModel::where('id', $id_shift)->value('jam_pulang');
+                        if($absen_time > $shift_pulang){
+                            $status = AbsenModel::where('tanggal', $absen_tanggal)->where('id_pegawai', $item[0])->value('status');
+                            if($status == "tepat_waktu"){
+                                AbsenModel::where('id', $check_id)->update([
+                                    'absen_pulang' => $absen_time,
+                                    'status' => 'tidak_tepat_waktu'
+                                ]);
                             }
-                            AbsenModel::where('id',$check_id)->update([
-                                'absen_pulang'=>$absen_time,
-                            ]);
+                        }
+                        AbsenModel::where('id', $check_id)->update([
+                            'absen_pulang' => $absen_time,
+                        ]);
                     }
                     else{
-                        $id_shift = KaryawanModel::where('id_absen',$item[0])->value('id_shift');
-                        $shift_masuk = ShiftModel::where('id',$id_shift)->value('jam_masuk');
+                        $id_shift = KaryawanModel::where('id_absen', $item[0])->value('id_shift');
+                        $shift_masuk = ShiftModel::where('id', $id_shift)->value('jam_masuk');
                         if($absen_time < $shift_masuk ){
-                            $absen->id_pegawai= $item[0];
+                            $absen->id_pegawai = $item[0];
                             $absen->tanggal = $absen_tanggal;
                             $absen->absen_masuk = $absen_time;
-                            $absen->status ='tidak_tepat_waktu';
+                            $absen->status = 'tidak_tepat_waktu';
                             $absen->save();
                         }
                         else{
-                            $absen->id_pegawai= $item[0];
+                            $absen->id_pegawai = $item[0];
                             $absen->tanggal = $absen_tanggal;
                             $absen->absen_masuk = $absen_time;
-                            $absen->status ='tepat_waktu';
+                            $absen->status = 'tepat_waktu';
                             $absen->save();
                         }
                     }
