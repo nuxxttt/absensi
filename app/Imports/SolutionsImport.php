c142493dd423696e73ea445eaf0a6eff5a06d945
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2023-10-14 23:14:16 +0700

    new fitur

--- app/Imports/SolutionsImport.php
+++ app/Imports/SolutionsImport.php
@@ -23,12 +23,11 @@ class SolutionsImport implements ToCollection
             $absen = new AbsenModel;
             if ($item[2] <= 1){
                 $absen_time = strtotime($item[1]);
-                $absen_time =  date('Y-m-d H:i:s', $absen_time);
-                $absen_time = date('H:i:s', $absen_time);
-                $absen_tanggal = date('Y-m-d',$absen_time);
-                $check_id = AbsenModel::where('tanggal',$absen_tanggal)->where('id_pegawai',$item[0])->value('id_pegawai');
-
                 if($absen_time !== false){
+                    $absen_time =  date('Y-m-d H:i:s', $absen_time);
+                    $absen_time = date('H:i:s', $absen_time);
+                    $absen_tanggal = date('Y-m-d',$absen_time);
+                    $check_id = AbsenModel::where('tanggal',$absen_tanggal)->where('id_pegawai',$item[0])->value('id_pegawai');
                     if(!empty($check_id)){
                         $id_shift = KaryawanModel::where('id_absen',$item[0])->value('id_shift');
                         $shift_pulang = ShiftModel::where('id',$id_shift)->value('jam_pulang');
