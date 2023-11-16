5496c112d5f4fbd3405aa977c8c3d6b94c4b9929
unknown <nustraproject@gmail.com>
2023-11-16 10:51:36 +0700

    new lembur status

--- app/Imports/SolutionsImport.php
+++ app/Imports/SolutionsImport.php
@@ -83,11 +83,11 @@ class SolutionsImport implements ToCollection
                             $absen->status = 'tidak_tepat_waktu';
                             $absen->save();
                         }
-                        elseif($khusus === "lapangan"){
+                        elseif($khusus == "lapangan"){
                             $absen->id_pegawai = $item[0];
                             $absen->tanggal = $absen_tanggal;
                             $absen->absen_masuk = $absen_time;
-                            $absen->status = 'lapangan'
+                            $absen->status = "lapangan"
                             $absen->save();
                         }
                         else{
